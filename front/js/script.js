05d2d59ae35bbc2e27975a1752eb08f0409ff50e
mserr113 <104605862+mserr113@users.noreply.github.com>
2022-09-20 19:49:15 -0400

    attempt at js

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,24 @@
-fetch("../back/package.json")
-.then(res => res.json())
-.then(data => console.log(data))
+async function populate() {
+
+    const requestURL = 'http://localhost:3000/api/products';
+    const request = new Request(requestURL);
+  
+    const response = await fetch(request);
+    const products = await response.json();
+  
+    populateProducts(products);
+
+    function populateProducts(obj) {
+        const section = document.queryselector('section');
+        const products = obj.products;
+
+        for (const product of products) {
+            const myArticle = document.createElement('article');
+            const myH3 = document.createElement('h3');
+            const myPara = document.createElement('p'); 
+
+            myArticle.appendChild(myH3);
+            myArticle.appendChild(myPara);
+        }
+    }
+}
