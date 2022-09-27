5b906fa498be9dd68a500acb8be9a04411576eff
mserr113 <104605862+mserr113@users.noreply.github.com>
2022-09-26 22:54:36 -0400

    added product to homepage

--- front/js/script.js
+++ front/js/script.js
@@ -2,23 +2,35 @@ async function populate() {
 
     const requestURL = 'http://localhost:3000/api/products';
     const request = new Request(requestURL);
-  
+
     const response = await fetch(request);
     const products = await response.json();
-  
+
     populateProducts(products);
 
-    function populateProducts(obj) {
-        const section = document.queryselector('section');
-        const products = obj.products;
-
-        for (const product of products) {
-            const myArticle = document.createElement('article');
-            const myH3 = document.createElement('h3');
-            const myPara = document.createElement('p'); 
-
-            myArticle.appendChild(myH3);
-            myArticle.appendChild(myPara);
-        }
-    }
 }
+
+function populateProducts(obj) {
+    const section = document.querySelector('section');
+    const item = obj.products
+
+    for (const item of obj) {
+        const myArticle = document.createElement('article');
+        const img = document.createElement('img');
+        const myH3 = document.createElement('h3');
+        const myPara = document.createElement('p'); 
+
+        img.src = item.imageUrl;
+        myH3.textContent = item.name;
+        myPara.textContent = item.description;
+
+        myArticle.appendChild(img);
+        myArticle.appendChild(myH3);
+        myArticle.appendChild(myPara);
+
+        section.appendChild(myArticle);
+    }
+
+}
+
+populate();
