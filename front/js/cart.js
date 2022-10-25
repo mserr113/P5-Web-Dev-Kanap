271ab3e1842ab947635c1931ba1d76570b0dae0d
mserr113 <104605862+mserr113@users.noreply.github.com>
2022-10-25 19:47:44 -0400

    added total price, move constants to be global

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,34 @@
 const cartItemsElement = document.getElementById('cart__items');
 
+const myArticle = document.createElement('article');
+myArticle.setAttribute("class","cart__item");
+//need to add data-id
+//need to add data-color
+const imgParentDiv = document.createElement('div');
+imgParentDiv.setAttribute("class","cart__item__img");
+const myImg = document.createElement('img');
+const itemContentDiv = document.createElement('div');
+itemContentDiv.setAttribute("class","cart__item__content");
+const descriptionParentDiv = document.createElement('div');
+descriptionParentDiv.setAttribute("class","cart__item__content__description");
+const myDescription = document.createElement('h2');
+const myColor = document.createElement('p');
+const myPrice = document.createElement('p');
+myPrice.setAttribute("class","itemPrice");
+const quantityContentDiv = document.createElement('div');
+quantityContentDiv.setAttribute("class","cart__item__content__settings");
+const quantityParentDiv = document.createElement('div');
+quantityParentDiv.setAttribute("class","cart__item__content__settings__quantity");
+const quantity = document.createElement('p');
+const myQuantity = document.createElement('INPUT');
+myQuantity.setAttribute("class","itemQuantity");
+myQuantity.setAttribute("name","itemQuantity");
+myQuantity.setAttribute("type", "number");
+const deleteParentDiv = document.createElement('div');
+deleteParentDiv.setAttribute("class","cart__item__content__settings__delete");
+const deleteItem = document.createElement('p');
+deleteItem.setAttribute("class","deleteItem");
+
 function displayCart() {
     let arrayInLocalStorage = localStorage.getItem("item"); 
     itemsInLocalStorage = JSON.parse(arrayInLocalStorage);
@@ -8,29 +37,17 @@ function displayCart() {
 }
 
 function populateCart() {
-        const myArticle = document.createElement('article');
-            myArticle.setAttribute("class","cart__item");
-            //need to add data-id
-            //need to add data-color
-        const imgParentDiv = document.createElement('div');
-            imgParentDiv.setAttribute("class","cart__item__img");
-        const myImg = document.createElement('img');
-        const itemContentDiv = document.createElement('div');
-            itemContentDiv.setAttribute("class","cart__item__content");
-        const descriptionParentDiv = document.createElement('div');
-            descriptionParentDiv.setAttribute("class","cart__item__content__description");
-        const myDescription = document.createElement('h2');
-        const myColor = document.createElement('p');
-        const myPrice = document.createElement('p');
-        const quantityContentDiv = document.createElement('div');
-        const quantityParentDiv = document.createElement('div');
-        const myQuantity = document.createElement('p');
-
         myImg.src = (itemsInLocalStorage[0].image);
         myDescription.textContent = (itemsInLocalStorage[0].title);
         myColor.textContent = (itemsInLocalStorage[0].color);
         myPrice.textContent = "€" + (itemsInLocalStorage[0].price);
-        //need to add quantitydropdown
+        quantity.textContent = "Quantity : "
+        myQuantity.setAttribute("value", itemsInLocalStorage[0].quantity);
+        //do I need the min max? I read 0-100 is the default.
+        //need to apply adjustment/message if more than 100 is entered.
+        myQuantity.setAttribute("min", "1");
+        myQuantity.setAttribute("max", "100");
+        deleteItem.textContent = "Delete";
 
         imgParentDiv.appendChild(myImg);
 
@@ -38,7 +55,15 @@ function populateCart() {
         descriptionParentDiv.appendChild(myColor);
         descriptionParentDiv.appendChild(myPrice);
 
+        quantityParentDiv.appendChild(quantity);
+        quantityParentDiv.appendChild(myQuantity);
+        quantityContentDiv.appendChild(quantityParentDiv);
+
+        deleteParentDiv.appendChild(deleteItem);
+
         itemContentDiv.appendChild(descriptionParentDiv);
+        itemContentDiv.appendChild(quantityContentDiv);
+        itemContentDiv.appendChild(deleteParentDiv);
 
         myArticle.appendChild(imgParentDiv);
         myArticle.appendChild(itemContentDiv);
@@ -46,6 +71,41 @@ function populateCart() {
         cartItemsElement.appendChild(myArticle);
     }
 
-
-
 displayCart();
+
+//Min-max for quantity field
+// document.getElementsByClassName("itemQuantity").addEventListener("change", function() {
+//     let v = parseInt(this.value);
+//     if (v < 1) this.value = 1;
+//     if (v > 50) this.value = 50;
+//   });
+
+//delete items from cart
+const deleteButtons = document.getElementsByClassName("deleteItem")
+for(var i = 0; i < deleteButtons.length; i++) {
+    var button = deleteButtons[i]
+    button.addEventListener('click', function(deleteFromCart) {
+        var buttonClicked = deleteFromCart.target;
+        buttonClicked.parentElement.parentElement.parentElement.remove();
+    })
+}
+
+//
+
+function updateCartTotal() {
+    var cartItemsSection = document.getElementById("cart__items")
+    var cartItemArticle = cartItemsSection.getElementsByClassName("cart__item")
+    var total = 0 
+        for (var i = 0; i < cartItemArticle.length; i++) {
+            var cartItem = cartItemArticle[i]
+            var priceElement = cartItem.getElementsByClassName('itemPrice')[0]
+            var quantityElement = cartItem.getElementsByClassName('itemQuantity')[0] 
+            var price = parseFloat(priceElement.innerText.replace('€ ',''))
+            var quantity = quantityElement.value
+            total = total + (price * quantity)        
+        }
+    document.getElementById('totalPrice').innerText = total 
+
+}
+
+updateCartTotal();
